befe6ceb9b07b783337730fba14695a44681c367
Victorkaris <victorkaris78@gmail.com>
2025-06-18 14:31:02 +0300

    Generate Trip using AI

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,22 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "j-mastery",
+  project: "travel-agency",
+  // An auth token is required for uploading source maps.
+  authToken: "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"
+  // ...
+};
+
+
+
 export default defineConfig(config => {
-  const sentryAuthToken = process.env.VITE_SENTRY_AUTH_TOKEN;
-
-  if (!sentryAuthToken) {
-    console.warn("⚠️ VITE_SENTRY_AUTH_TOKEN is not defined — Sentry source maps will not be uploaded.");
-  }
-
-  const sentryConfig: SentryReactRouterBuildOptions = {
-    org: "j-mastery",
-    project: "travel-agency",
-    authToken: sentryAuthToken, // Use process.env here
-  };
-
   return {
-    plugins: [
-      tailwindcss(),
-      tsconfigPaths(),
-      reactRouter(),
-      sentryReactRouter(sentryConfig, config),
-    ],
-    ssr: {
-      noExternal: [/@syncfusion/],
-    },
+  plugins: [tailwindcss(),  tsconfigPaths(), reactRouter(),sentryReactRouter(sentryConfig, config)],
+  sentryConfig,
+   ssr: {
+    noExternal: [/@syncfusion/] 
+  },
   };
 });
