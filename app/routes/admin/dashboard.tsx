8c65616791292f6b81516d250cb1996a830a4b9e
Victorkaris <victorkaris78@gmail.com>
2025-06-24 01:57:07 +0300

    update dashboard fo fetch realtime data

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,20 +1,82 @@
-import { StatsCard, TripCard } from "components";
-import Header from "../../../components/Header";
-import { dashboardStats, user,  allTrips } from "~/constants";
-import { getUser } from "~/appwrite/auth";
+import { Header, StatsCard, TripCard } from "components";
+import { getAllUsers, getUser } from "~/appwrite/auth";
 import type { Route } from "./+types/dashboard";
-
-const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
+import { getTripsByTravelStyle, getUserGrowthPerDay, getUsersAndTripsStats } from "~/appwrite/dashboard";
+import { getAllTrips } from "~/appwrite/trips";
+import { parseTripData } from "~/lib/utils";
+import { Category, ChartComponent, ColumnSeries, DataLabel, Inject, SeriesCollectionDirective, SeriesDirective, SplineAreaSeries, Tooltip } from "@syncfusion/ej2-react-charts";
+import {tripXAxis, tripyAxis, userXAxis, useryAxis} from "~/constants";
+import {GridComponent, ColumnsDirective, ColumnDirective} from "@syncfusion/ej2-react-grids";
 
 // export async function clientLoader () {
 //     return await getUser();
 // } ;oiuytre
 
 
-export const clientLoader = async () => await getUser();
+export const clientLoader = async () => {
+    const [ 
+        user, 
+        dashboardStats,
+        trips,
+        userGrowth,
+        tripsByTravelStyle,
+        allUsers,
+        ] = await Promise.all([
+         await getUser(),
+        await getUsersAndTripsStats(),
+        await getAllTrips(4, 0),
+        await getUserGrowthPerDay(),
+        await getTripsByTravelStyle(),
+        await getAllUsers(4,0),
+    ]) 
+
+    const allTrips = trips.allTrips.map(({$id, tripDetail, imageUrl})=> ({
+        id: $id,
+        ...parseTripData(tripDetail),
+        imageUrl: imageUrl ?? []
+    }))
+
+const mappedUsers: UsersItineraryCount[] = allUsers.users.map((user) => ({
+    imageUrl: user.imageUrl,
+    name: user.name,
+    count: user.itineraryCount ?? Math.floor(Math.random() * 10),
+}))    
+
+    return{ 
+        user, 
+        dashboardStats,
+        allTrips,
+        userGrowth,
+        tripsByTravelStyle,
+        allUsers: mappedUsers
+    }
+   
+}
 
 const dashboard = ({ loaderData }: Route.ComponentProps) => {
-    const user = loaderData as User | null;
+    const user = loaderData.user as User | null;
+    const { dashboardStats, allTrips, userGrowth, tripsByTravelStyle, allUsers } = loaderData;
+    
+    const trips = allTrips.map((trip) => ({
+        imageUrl: trip.imageUrl[0],
+        name: trip.name,
+        interests: trip.interests,
+    }))
+
+    const usersAndTrips = [
+        {
+            title: 'Latest User Signups',
+            dataSource: allUsers,
+            field: 'count',
+            headerText: 'Trips created'
+        },
+        {
+            title: 'Trips based on interests',
+            dataSource: trips,
+            field: 'interest',
+            headerText: 'interests'
+        }
+    ]
 
   return (
     <main className="dashboard wrapper">
@@ -28,22 +90,22 @@ const dashboard = ({ loaderData }: Route.ComponentProps) => {
 
           <StatsCard 
             headerTitle="Total Users"
-            total={totalUsers}
-            currentMonthCount={usersJoined.currentMonth}
-            lastMonthCount={usersJoined.lastMonth}
+            total={dashboardStats.totalUsers}
+            currentMonthCount={dashboardStats.usersJoined.currentMonth}
+            lastMonthCount={dashboardStats.usersJoined.lastMonth}
           />
 
           <StatsCard 
             headerTitle="Total Trips"
-            total={totalTrips}
-            currentMonthCount={tripsCreated.currentMonth}
-            lastMonthCount={tripsCreated.lastMonth}
+            total={dashboardStats.totalTrips}
+            currentMonthCount={dashboardStats.tripsCreated.currentMonth}
+            lastMonthCount={dashboardStats.tripsCreated.lastMonth}
           />
           <StatsCard 
             headerTitle="Active Users Today"
-            total={userRole.total}
-            currentMonthCount={userRole.currentMonth}
-            lastMonthCount={userRole.lastMonth}
+            total={dashboardStats.userRole.total}
+            currentMonthCount={dashboardStats.userRole.currentMonth}
+            lastMonthCount={dashboardStats.userRole.lastMonth}
           />
 
         </div>
@@ -54,19 +116,101 @@ const dashboard = ({ loaderData }: Route.ComponentProps) => {
             <h1 className="text-xl font-semibold text-dark"> Created Trips</h1>
 
             <div className="trip-grid">
-                {allTrips.slice(0,4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
-                    <TripCard
-                        key={id}
-                        id={id.toString()}
-                        name={name}
-                        imageUrl={imageUrls[0]}
-                        location={itinerary?.[0]?.location ?? " "}
-                        tags={tags}
-                        price={estimatedPrice}
-                     />))}
+                {allTrips.map((trip) => (
+                    <TripCard 
+                        key={trip.id} // Use $id as the unique identifier from Appwrite
+                        id={trip.id.toString()}
+                        name={trip.name!}
+                        location={trip.itinerary?.[0]?.location ?? ''}
+                        imageUrl={trip.imageUrl[0]}
+                        tags={[trip.interests!, trip.travelStyle!]}
+                        price={trip.estimatedPrice!} // Use estimatedPrice from each trip
+                    />))}
             </div>
 
         </section>
+
+        <section className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+            <ChartComponent
+                id="chart-1"
+                primaryXAxis={userXAxis}
+                primaryYAxis={useryAxis}
+                title="User Growth"
+                tooltip={{ enable: true}}
+                >
+                <Inject services={[ColumnSeries, SplineAreaSeries, Category, DataLabel, Tooltip]} />
+                <SeriesCollectionDirective>
+                    <SeriesDirective
+                        dataSource={userGrowth}
+                        xName="day"
+                        yName="count"
+                        type="Column"
+                        name="Column"
+                        columnWidth={0.3}
+                        cornerRadius={{topLeft: 10, topRight: 10}}
+                    />
+                    <SeriesDirective
+                        dataSource={userGrowth}
+                        xName="day"
+                        yName="count"
+                        type="SplineArea"
+                        name="Wave"
+                        fill="rgba(71, 132, 238, 0.3)"
+                        border={{width:2, color: '#4784EE'}}
+                    />
+                </SeriesCollectionDirective>
+            </ChartComponent>
+            <ChartComponent
+                id="chart-2"
+                primaryXAxis={tripXAxis}
+                primaryYAxis={tripyAxis}
+                title="Trip Trends"
+                tooltip={{ enable: true}}
+                >
+                <Inject services={[ColumnSeries, SplineAreaSeries, Category, DataLabel, Tooltip]} />
+                <SeriesCollectionDirective>
+                    <SeriesDirective
+                        dataSource={tripsByTravelStyle}
+                        xName="travelStyle"
+                        yName="count"
+                        type="Column"
+                        name="day"
+                        columnWidth={0.3}
+                        cornerRadius={{topLeft: 10, topRight: 10}}
+                    />
+                </SeriesCollectionDirective>
+            </ChartComponent>
+        </section>
+    </section>
+
+    <section className='user-trip wrapper'>
+        {usersAndTrips.map(({title, dataSource, field, headerText}, i) => (
+            <div key={i} className='flex flex-col gap-5'>
+                <h3 className="p-20-semibold text-dark-100">{title}</h3>
+                <GridComponent dataSource={dataSource} gridLines='None'>
+                        <ColumnsDirective>
+                            <ColumnDirective
+                                field='name'
+                                headerText='Name'
+                                width='150'
+                                textAlign='Left'
+                                template={(props: UserData) => (
+                                    <div className='flex items-center gap-1.5 px-4'>
+                                        <img src={props.imageUrl} alt='user' className='rounded-full size-8 aspect-square' referrerPolicy='no-referrer'/>
+                                        <span>{props.name}</span>
+                                    </div>
+                                )}
+                            />
+                            <ColumnDirective
+                                field={field}
+                                headerText={headerText}
+                                width='150'
+                                textAlign="Left"
+                            />
+                        </ColumnsDirective>
+                    </GridComponent>
+            </div>
+        ))}    
     </section>
 
     </main>
--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -2,7 +2,9 @@ import { Header, TripCard } from "components"
 import type { Route } from "./+types/trips";
 import { getAllTrips, getTripById } from "~/appwrite/trips";
 import { parseTripData } from "~/lib/utils";
-import type { LoaderFunctionArgs } from "react-router";
+import { useSearchParams, type LoaderFunctionArgs } from "react-router";
+import { useState } from "react";
+import { PagerComponent } from "@syncfusion/ej2-react-grids";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
     const limit = 8;
@@ -25,6 +27,16 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 const Trips = ({ loaderData } : Route.ComponentProps) => {
     const trips = loaderData.trips as Trip[] | [];
 
+    const [searchParams] = useSearchParams();
+    const initialPage = Number(searchParams.get('page') || '1')
+
+    const [currentPage, setCurrentPage] = useState(initialPage);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+        window.location.search = `?page=${page}`
+    }
+
     return (
         <main className='all-users wrapper'>
         <Header 
@@ -35,11 +47,11 @@ const Trips = ({ loaderData } : Route.ComponentProps) => {
             />
 
             <section>
-                <h1 className='p-24-semibold text-dark-100'>
+                <h1 className='p-24-semibold text-dark-100 mb-4'>
                     Manage Created Trips
                 </h1>
 
-                <div className='trip-grid'>
+                <div className='trip-grid mb-4'>
                     {trips.map((trip) => (
                     <TripCard 
                         key={trip.id} // Use $id as the unique identifier from Appwrite
@@ -52,6 +64,14 @@ const Trips = ({ loaderData } : Route.ComponentProps) => {
                     />
                     ))}
                 </div>
+
+                <PagerComponent
+                    totalRecordsCount = {loaderData.total}
+                    pageSize={8}
+                    currentPage={currentPage}
+                    click={(args) => handlePageChange(args.currentPage)}
+                    cssClass='!mb-4'
+                />
             </section>
 
         </main>
